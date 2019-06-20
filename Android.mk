163ae6b6f63f5bdfb6f5e45435ea74f8d7791690
lilindian16 <jaimesequeira14@gmail.com>
2019-06-20 23:36:57 +1200

    removed unnecessary lines in android.mk

--- Android.mk
+++ Android.mk
@@ -185,31 +185,4 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := qdcm_calib_data_hx8394F_HD720p_video_BOE_c3a
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/etc/qdcm_calib_data_hx8394F_HD720p_video_BOE_c3a.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_SUFFIX := .xml
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := qdcm_calib_data_ili9881c_HD720p_video_Ebbg_c3a
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/etc/qdcm_calib_data_ili9881c_HD720p_video_Ebbg_c3a.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_SUFFIX := .xml
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := qdcm_calib_data_nt35521s_HD720p_video_EBBG_c3a
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/etc/qdcm_calib_data_nt35521s_HD720p_video_EBBG_c3a.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_SUFFIX := .xml
-include $(BUILD_PREBUILT)
-
 endif
